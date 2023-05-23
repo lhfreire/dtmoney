bed40c7fdf03b034dcec1e676bfb8c25a3050412
Luis Freire <luis.henrique.freire@gmail.com>
2023-05-23 18:49:44 -0300

    ending dtmoney

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import Modal from 'react-modal';
-import { FormEvent, useState, useContext } from 'react';
-import { api } from '../../services/api';
-import { TransactionsContext } from '../../TransactionsContext';
+import { FormEvent, useState } from 'react';
+import { useTransactions } from '../../hooks/useTransactions';
+
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/entradas.svg';
@@ -15,7 +15,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,23 +1,30 @@
-import React, { useContext } from 'react';
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saidas.svg'
 import totalImg from '../../assets/total.svg'
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from "./styles";
-import { TransactionsContext } from '../../TransactionsContext';
 
 export function Summary() {
 
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions();
 
-    const totalDeposits = transactions.reduce((acc, transaction) => {
-        if (transaction.type == 'deposit') {
-            return acc + transaction.amount;
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type == 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
         }
 
         return acc;
-    }, 0);
-
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    })
+        
     return (
         <Container>
             <div>
@@ -25,21 +32,36 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>{totalDeposits}</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
             </div>
             <div>
                 <header>
                     <p>Saidas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>- R$500,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
             <div className="highLight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     );
